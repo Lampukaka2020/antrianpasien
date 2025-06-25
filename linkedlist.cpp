1677c14e5b6405ce679db0e915b4eb48bd31344a
Lampukaka2020 <helman.hrr@gmail.com>
2025-06-25 16:02:02 +0800

    Pointer untuk menunjuk ke pasien pertama

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -6,3 +6,5 @@ struct Pasien {
     string nama;       // Nama pasien
     Pasien* next;      // Pointer ke pasien berikutnya
 };
+// Pointer global untuk menunjuk ke pasien pertama (kepala antrian)
+Pasien* head = nullptr;
