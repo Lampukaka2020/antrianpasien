bdfdca3ddc9a27265822d079cf28a77f610c9669
Lampukaka2020 <helman.hrr@gmail.com>
2025-06-25 16:04:25 +0800

    untuk menambahkan pasien

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -8,3 +8,20 @@ struct Pasien {
 };
 // Pointer global untuk menunjuk ke pasien pertama (kepala antrian)
 Pasien* head = nullptr;
+// Fungsi untuk menambahkan pasien ke antrian
+void tambahPasien(string nama) {
+    // Buat node baru
+    Pasien* baru = new Pasien{nama, nullptr};
+
+    if (head == nullptr) {
+        // Jika antrian kosong, pasien pertama langsung jadi head
+        head = baru;
+    } else {
+        // Jika tidak kosong, cari pasien terakhir, lalu tambahkan setelahnya
+        Pasien* temp = head;
+        while (temp->next != nullptr)
+            temp = temp->next;
+        temp->next = baru;
+    }
+    cout << "Pasien \"" << nama << "\" ditambahkan ke antrian.\n";
+}
